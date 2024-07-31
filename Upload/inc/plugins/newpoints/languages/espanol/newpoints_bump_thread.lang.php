d6c8ad1391c87e2fce7468f42034812d815b3f31
Sama34 <Sama34@users.noreply.github.com>
2024-07-30 22:54:07 -0700

    Code style PSR12

--- Upload/inc/plugins/newpoints/languages/espanol/newpoints_bump_thread.lang.php
+++ Upload/inc/plugins/newpoints/languages/espanol/newpoints_bump_thread.lang.php
@@ -3,29 +3,28 @@
 /***************************************************************************
  *
  *   Newpoints Bump Thread plugin (/inc/plugins/newpoints/languages/espanol/npbumpthread.php)
- *	 Author: Omar Gonzalez
+ *     Author: Omar Gonzalez
  *   Copyright: © 2012 Omar Gonzalez
- *   
+ *
  *   Website: http://community.mybb.com/user-25096.html
  *
  *   Allows users to bump their own threads without postingon exchange of points.
  *
  ***************************************************************************
- 
-****************************************************************************
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-	
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-****************************************************************************/
+ ****************************************************************************
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ****************************************************************************/
 
 $l['bt_plugin'] = 'Refrescar Temas';
 
